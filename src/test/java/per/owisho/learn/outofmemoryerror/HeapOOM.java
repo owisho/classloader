63b93770664d812d60936991a2c03fa645f284c3
owisho <owisho2012@126.com>
2016-06-16 01:56:46 +0800

    提交堆内存溢出 调试设置的虚拟机参数 -verbose:gc -Xms20M -Xmx20M -Xmn10M -XX:+PrintGCDetails -XX:SurvivorRatio=8

---
+++ src/test/java/per/owisho/learn/outofmemoryerror/HeapOOM.java
@@ -0,0 +1,26 @@
+package per.owisho.learn.outofmemoryerror;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * VM Args:-Xms20m -Xmx20m -XX:+HeapDumpOnOutOfMemoryError
+ * @author owisho
+ *
+ */
+public class HeapOOM {
+
+	static class OOMObject{
+		
+	}
+	
+	public static void main(String[] args) {
+		List<OOMObject> list = new ArrayList<OOMObject>();
+		
+		while (true) {
+			list.add(new OOMObject());
+		}
+		
+	}
+	
+}
