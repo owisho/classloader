694ea7724fb6125e5f543f6efcd521460a72d728
owisho <owisho2012@126.com>
2017-01-12 14:50:11 +0800

    提交测试类加载器加载的类对其他类的可见性

--- src/test/java/per/owisho/learn/pluginclassloader/PluginClassVisibility.java
+++ src/test/java/per/owisho/learn/pluginclassloader/PluginClassVisibility.java
@@ -1,8 +1,13 @@
 package per.owisho.learn.pluginclassloader;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.lang.reflect.Method;
+import java.net.JarURLConnection;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 
 /**
@@ -14,7 +19,10 @@ import java.net.URLClassLoader;
 public class PluginClassVisibility {
 
 	public static void main(String[] args) throws Exception {
-		
+		cannotfindsysclass();
+	}
+	
+	public static void canfindsysclass() throws Exception{
 		URL url = new URL("file:\\D:\\workspace\\java\\learn\\classloader\\extrajar\\plugin.jar");
 		URLClassLoader loader = new URLClassLoader(new URL[]{url});
 		Class<?> cls = loader.loadClass("Plugin");
@@ -23,17 +31,64 @@ public class PluginClassVisibility {
 		Method method = cls.getMethod("findClass", String.class);
 		Object obj = method.invoke(cls.newInstance(), "per.owisho.learn.pluginclassloader.PluginClassVisibility");
 		System.out.println(obj);
+	}
+	
+	public static void canfindpluginclass(){
 		PluginClassVisibility p = new PluginClassVisibility();
 		try {
 			p.findClass("Plugin");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+	}
+	
+	public static void cannotfindsysclass() throws Exception{
+		URL url = new URL("jar",null,"file:\\D:\\workspace\\java\\learn\\classloader\\extrajar\\plugin.jar!/");
+		SpecialClassLoader loader = new SpecialClassLoader(url);
+		Class<?> cls = loader.loadClass("Plugin");
+		System.out.println(cls);
+		Method method = cls.getMethod("findClass", String.class);
+		Object obj = method.invoke(cls.newInstance(), "per.owisho.learn.pluginclassloader.PluginClassVisibility");
+		System.out.println(obj);
 	}
 	
 	public Class<?> findClass(String className) throws ClassNotFoundException{
 		return Class.forName(className);
+	}
+	
+}
+
+/**
+ * 不使用父类加载器查找类的加载器
+ * @author owisho
+ *
+ */
+class SpecialClassLoader extends ClassLoader{
+	
+	private URL url;//网络地址的根路径
+	
+	public SpecialClassLoader(URL url){
+		super(Thread.currentThread().getContextClassLoader().getParent());
+		this.url = url;
+	}
+	
+	@Override
+	protected Class<?> findClass(String name) throws ClassNotFoundException {
+		try {
+			JarURLConnection jarconnection = (JarURLConnection)url.openConnection();
+			JarFile jar = jarconnection.getJarFile();
+			JarEntry entry = jar.getJarEntry(name+".class");
+			InputStream in = jar.getInputStream(entry);
+			ByteArrayOutputStream b = new ByteArrayOutputStream();
+			int read = -1;
+			while((read=in.read())!=-1){
+				b.write(read);
+			}
+			return defineClass(name, b.toByteArray(), 0, b.size());
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new ClassNotFoundException();
+		}
 	}
 	
 }
